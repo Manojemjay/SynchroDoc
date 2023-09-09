6035fd20ed826c49617dfab00a6e1bf8b2c4f97e
Manojemjay <manojjmanu7@gmail.com>
2023-09-09 14:41:25 +0530

    settin up quill

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
 
 function TextEditor() {
+  useEffect(() => {
+    new Quill("#container",{theme: "snow"})
+  },[])
   return (
-    <div>TextEditor</div>
+    <div id="container"></div>
   )
+  //sdasa
 }
 
 export default TextEditor
