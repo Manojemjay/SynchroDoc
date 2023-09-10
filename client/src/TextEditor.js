985965d429cbffcd990b76a05f43fc425ab13c1e
Manojemjay <manojjmanu7@gmail.com>
2023-09-11 00:39:08 +0530

    simultaneous typing is working using unique ids

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import {io} from 'socket.io-client'
+import { useParams } from 'react-router-dom'
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }], 
@@ -14,11 +15,30 @@ const TOOLBAR_OPTIONS = [
   ["image", "blockquote", "code-block"], 
   ["clean"],
 ]
-
+ 
 function TextEditor() {
-
+  const {id: documentId} = useParams()
   const [socket, setSocket] = useState()
   const [quill, setQuill] = useState()
+
+  // console.log(documentId)
+
+  
+
+  useEffect(() => {
+    if(socket == null || quill == null) return 
+
+    socket.once("load-document", document => {
+      quill.setContents(document);
+      quill.enable();
+    })
+
+    socket.emit('get-document', documentId);
+    
+  }, [socket, quill, documentId])
+
+
+
   useEffect (() => { 
     const s = io("http://localhost:3001")
     setSocket(s)
@@ -28,15 +48,49 @@ function TextEditor() {
     }
   },[])
 
+
+
+
+  useEffect (() => {
+    if (socket == null || quill == null) return
+
+    const handler = (delta) => { 
+      quill.updateContents(delta)
+    }
+    socket.on("receive-changes", handler)
+
+    return () => {
+    socket.off("receive-changes", handler)
+    }
+  }, [socket, quill])
+
+
+  useEffect (() => {
+    if (socket == null || quill == null) return
+
+    const handler = (delta, oldDelta, source) => { 
+      if (source !== 'user') return 
+      socket.emit("send-changes", delta)
+    }
+    quill.on('text-change', handler)
+
+    return () => {
+      quill.off('text-change', handler)
+    }
+  }, [socket, quill])
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return
+
     wrapper.innerHTML = ''
-    const editor = document.createElement('div')
+    const editor = document.createElement("div")
     wrapper.append(editor)
-    const q = new Quill(editor,{theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+    const q = new Quill(editor,{theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS},})
+    q.disable()
+    q.setText('Loading...')
     setQuill(q)
   },[])
-  //sdasdasd
+  
   return (
     <div className="container" ref ={wrapperRef}></div>
   )
--- server/server.js
+++ server/server.js
@@ -7,5 +7,15 @@ const io = require("socket.io") (3001, {
 })
 
 io.on("connection", socket => {
-    console.log("connected")
+
+    socket.on("get-document", documentId => {
+        const data = ""
+        socket.join(documentId)
+        socket.emit("load-document", data)
+
+        socket.on("send-changes",  delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)    
+    })          
+  })
 })
+
